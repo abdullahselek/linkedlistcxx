8bcb641bce08032d95d425a8c5c8552269911514
Abdullah Selek <abdullahselek@gmail.com>
2017-05-22 10:05:19 +0200

    create files

---
+++ linkedlistcxx.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Selek, Abdullah on 22.05.17.
+//
+
---
+++ linkedlistcxx.h
@@ -0,0 +1,8 @@
+//
+// Created by Selek, Abdullah on 22.05.17.
+//
+
+#ifndef LINKEDLISTCXX_LINKEDLISTCXX_H
+#define LINKEDLISTCXX_LINKEDLISTCXX_H
+
+#endif //LINKEDLISTCXX_LINKEDLISTCXX_H
